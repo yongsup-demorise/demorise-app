c0096c0c7b525d46fdfdfaeb316a7d4e1fd109a5
yongsup-demorise <aa56235981@gmail.com>
2026-08-15 13:40:49 +0900

    Delete src/src/pages directory

--- src/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>홈 화면</h1>
-      <p>여기가 앱의 첫 화면입니다.</p>
-    </div>
-  );
-}
-
--- src/src/pages/Login.js
+++
@@ -1,9 +0,0 @@
-export default function Login() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>로그인 화면</h1>
-      <p>여기에 로그인 UI가 들어갈 예정입니다.</p>
-    </div>
-  );
-}
-
